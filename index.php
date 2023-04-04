0d7e8ba04bdfd57bb41ec53689fa5c2e98cc951f
Evgeny <76429147+PozdnyshevEE@users.noreply.github.com>
2023-04-05 00:22:48 +0300

    gender commit

--- index.php
+++ index.php
@@ -3,28 +3,62 @@
 
     function getPartsFromFullname($str) {
 
-            $key = ['surname', 'name', 'patronomyc'];
-            
-            $new_array = explode(' ', $str);
-            $result = array_combine($key, $new_array);
+        $key = ['surname', 'name', 'patronomyc'];
+        $new_array = explode(' ', $str);
+        $result = array_combine($key, $new_array);
         
         return $result;
         
     }
 
     function getFullnameFromParts($surname, $name, $patronomyc) {
+
         $result = $surname . ' ' . $name . ' ' . $patronomyc;
+        
         return $result;
     }
 
     function getShortName($str) {
-            $array = getPartsFromFullname($str);
-            $temp_surname = mb_substr($array['surname'], 0, 1);
-            $result = $array['name'] . ' ' . $temp_surname . '.';
-            return $result;
-     }
 
-    $array = getPartsFromFullname($example_persons_array[2]['fullname']);
-    print_r(getShortName($example_persons_array[2]['fullname']));
+        $array = getPartsFromFullname($str);
+        $temp_surname = mb_substr($array['surname'], 0, 1);
+        $result = $array['name'] . ' ' . $temp_surname . '.';
+        
+        return $result;
+    }
+
+    function getGenderFromName($str) {
+        
+        $array = getPartsFromFullname($str);
+        $totalSexAttribute = 0;
+        $patronomyc = $array['patronomyc'];
+        $name = $array['name'];
+        $surname = $array['surname'];
+
+        if ((mb_substr($patronomyc, -3)) === 'вна') {
+            $totalSexAttribute -= 1;
+        }
+        else if ((mb_substr($patronomyc, -2)) === 'ич') {
+            $totalSexAttribute += 1;
+        }
+        if ((mb_substr($name, -1)) === 'а') {
+            $totalSexAttribute -= 1;
+        }
+        else if (((mb_substr($name, -1)) === 'й') || ((mb_substr($name, -1)) === 'н')) {
+            $totalSexAttribute += 1;
+        }
+        if ((mb_substr($surname, -2)) === 'ва') {
+            $totalSexAttribute -= 1;
+        }
+        else if ((mb_substr($surname, -1)) === 'в') {
+            $totalSexAttribute += 1;
+        }
+
+        $totalSexAttribute = ($totalSexAttribute <=> 0);
+
+        return $totalSexAttribute;
+    }
+
+    print_r(getGenderFromName($example_persons_array[1]['fullname']));
     
 ?>
